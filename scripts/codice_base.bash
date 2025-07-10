5a98f0bf5d30ae27aa1e214c4fe0ae9b17d58185
nicolamiotto <miottonicola03@gmail.com>
2025-07-10 10:23:43 +0200

    add argument -c | --check to check the dependency

--- scripts/codice_base.bash
+++ scripts/codice_base.bash
@@ -39,11 +39,15 @@ function ha_genitore_accessibile() {            # Controlla se il file genitore 
 
 function requisiti() {
     local type="$1"
+    local check="$2"
     if ! command -v "$type" &>/dev/null; then
-        printlines \
-            "$(come_errore "Errore: requisiti di sistema non soddisfatti.")" \
-            "$(come_avviso "Impossibile proseguire senza comando $type.")"
-        sleep 2
+        if [[ ! "$check" == "check" ]]; then
+            println "$(come_errore "Errore: requisiti di sistema non soddisfatti.")"
+            println "$(come_avviso "Impossibile proseguire senza comando $type.")"
+            sleep 2
+        else
+            println "$(come_errore "Requisiti di sistema non soddisfatti: comando $type mancante.")"
+        fi
         return 1
     fi
     return 0
